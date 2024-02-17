ac7706120b967448d43c44811319ae8c57283863
akashmumana <akashmuman@gmail.com>
2024-02-17 18:55:43 +0530

    buildcommit

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userUpdateRouter from './router/user.router.js'
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import listingrouter from "./router/listingRoute.js"
+import path from 'path'
 
 dotenv.config()
 
@@ -15,7 +16,8 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 }).catch((err) => {
     console.log(err)
 })
-
+ 
+const _dirname = path.resolve()
 
 app.use(express.json())
 app.use(cookieParser())
@@ -23,6 +25,12 @@ app.use(cookieParser())
 app.use("/api/auth", userRouter)
 app.use('/api/user',userUpdateRouter)
 app.use('/api/listing',listingrouter)
+
+app.use(express.static(path.join(_dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(_dirname, 'client','dist','index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statuscode = err.statuscode || 500
